383cbd00130d40017d2901c69b0ef6fcf95505a8
Ferdi Dirgantara <ferdifordev@gmail.com>
2020-01-05 13:26:26 +0700

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+@section('content')
+
+<h1>Home Page</h1>
+
+@if(session('successMsg'))
+<div class="alert alert-success" role="alert">
+  {{ session('successMsg') }}
+</div>
+
+@endif
+
+
+@endsection
